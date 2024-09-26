02daea6a6a020b1b60187dd5c1938dd5d69ebc6f
maniek <mpastuszka@gmail.com>
2024-09-26 15:36:00 +0200

    [add] - put endpoint implemented

--- src/main/java/com/vavatech/rest/restdemo/rest/CarRestController.java
+++ src/main/java/com/vavatech/rest/restdemo/rest/CarRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,6 +32,7 @@ public class CarRestController {
 		);
 	}
 
+	// TODO: handle 404
 	// /cars/{id}
 	@GetMapping(path = "/{id}")
 	public CarRecord getCarById(@PathVariable("id") Integer idik) {
@@ -65,5 +67,19 @@ public class CarRestController {
 			return ResponseEntity.noContent().build();
 		}
 		return ResponseEntity.notFound().build();
+	}
+
+	@PutMapping(path = "/{id}")
+	public CarDtoResponse updateCarById(@PathVariable("id") int id, @RequestBody CarRecord car) {
+		log.info("trying to update car with id = [{}]", id);
+
+		// call service
+
+		return CarDtoResponse.builder()
+				.id(id)
+				.brand(car.brand())
+				.year(car.year())
+				.model(car.model())
+				.build();
 	}
 }
