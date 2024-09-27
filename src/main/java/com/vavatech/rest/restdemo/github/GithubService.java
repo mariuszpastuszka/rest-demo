8e6fe1545897c4190ba1906edbdbbe1fefc46f5b
maniek <mpastuszka@gmail.com>
2024-09-27 15:31:50 +0200

    [add] - GithubService in progress...

--- src/main/java/com/vavatech/rest/restdemo/github/GithubService.java
+++ src/main/java/com/vavatech/rest/restdemo/github/GithubService.java
@@ -1,16 +1,22 @@
 package com.vavatech.rest.restdemo.github;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class GithubService {
 	private final RestTemplate restTemplate;
 	private final String url;
 
-	public GithubService(RestTemplateBuilder restTemplateBuilder, String url) {
+	public GithubService(RestTemplateBuilder restTemplateBuilder, @Value("${github.repositories.url}") String url) {
 		this.restTemplate = restTemplateBuilder.build();
+		this.url = url;
+
+		log.info("url = [{}]", url);
 	}
 
 	public String getMyRepositories() {
