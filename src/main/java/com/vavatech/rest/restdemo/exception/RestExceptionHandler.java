38bbda74dd09ba1598768a9ea54bcd564acd002d
maniek <mpastuszka@gmail.com>
2024-09-27 14:43:17 +0200

    [refactor] - ExceptionHandler refactoring - in progress...

--- src/main/java/com/vavatech/rest/restdemo/exception/RestExceptionHandler.java
+++ src/main/java/com/vavatech/rest/restdemo/exception/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.vavatech.rest.restdemo.exception;
 
 import com.vavatech.rest.restdemo.car.CarNotFoundException;
 import com.vavatech.rest.restdemo.car.CarValidationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,14 +10,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class RestExceptionHandler {
 
-	@ExceptionHandler({CarNotFoundException.class, CarValidationException.class})
-	public String handleCarNotFoundException(Exception ex) {
+	@ExceptionHandler({CarNotFoundException.class, CarValidationException.class, MethodArgumentNotValidException.class})
+	public ResponseEntity<ExceptionRecord> handleCarNotFoundException(Exception ex) {
 
 		return switch (ex) {
-			case CarNotFoundException exception -> "car not found:(";
-			case CarValidationException exception -> "car validation failed:(";
-			case MethodArgumentNotValidException exception -> "validation failed:(";
-			case Exception exception -> "unknown exception:(";
+			case CarNotFoundException exception -> ResponseEntity.notFound().build();
+			case CarValidationException exception -> ResponseEntity.badRequest().build();
+			case MethodArgumentNotValidException exception -> ResponseEntity.badRequest().build();
+			case Exception exception -> ResponseEntity.badRequest().build();
 		};
 	}
 
