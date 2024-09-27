55000f2a66628c0ad60e32378a0f6c916c694b02
maniek <mpastuszka@gmail.com>
2024-09-27 15:54:16 +0200

    [add] - fixed

--- src/main/java/com/vavatech/rest/restdemo/github/GithubService.java
+++ src/main/java/com/vavatech/rest/restdemo/github/GithubService.java
@@ -24,7 +24,7 @@ public class GithubService {
 		log.info("url = [{}]", url);
 		ParameterizedTypeReference<List<String>> typeRef = new ParameterizedTypeReference<List<String>>() {};
 	}
-// TODO: fixme :)
+
 	public List<Repository> getMyRepositories() {
 		return restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Repository>>(){}).getBody();
 	}
