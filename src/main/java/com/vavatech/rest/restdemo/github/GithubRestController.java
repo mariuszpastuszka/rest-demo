353159a561e3dd04bb46ea335f5e3b13b6814e9f
maniek <mpastuszka@gmail.com>
2024-09-27 15:23:59 +0200

    [add] - GithubService in progress...

--- src/main/java/com/vavatech/rest/restdemo/github/GithubRestController.java
+++ src/main/java/com/vavatech/rest/restdemo/github/GithubRestController.java
@@ -1,15 +1,24 @@
 package com.vavatech.rest.restdemo.github;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/github")
+@Slf4j
 class GithubRestController {
 
 	private final GithubService githubService;
 
 	GithubRestController(GithubService githubService) {
 		this.githubService = githubService;
+	}
+
+	@GetMapping("/repositories")
+	String getMyRepositories() {
+		log.info("List my repositories");
+		return githubService.getMyRepositories();
 	}
 }
--- src/main/java/com/vavatech/rest/restdemo/github/GithubService.java
+++ src/main/java/com/vavatech/rest/restdemo/github/GithubService.java
@@ -1,8 +1,18 @@
 package com.vavatech.rest.restdemo.github;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GithubService {
+	private final RestTemplate restTemplate;
 
+	public GithubService(RestTemplateBuilder restTemplateBuilder) {
+		this.restTemplate = restTemplateBuilder.build();
+	}
+
+	public String getMyRepositories() {
+		return null;
+	}
 }
