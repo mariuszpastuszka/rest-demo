c7ebdfe2b25c5370be0d9ea9ff4f1b3463c73b02
maniek <mpastuszka@gmail.com>
2024-09-27 15:20:31 +0200

    [add] - github feature started

--- src/main/java/com/vavatech/rest/restdemo/exception/RestExceptionHandler.java
+++ src/main/java/com/vavatech/rest/restdemo/exception/RestExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
---
+++ src/main/java/com/vavatech/rest/restdemo/github/GithubService.java
@@ -0,0 +1,8 @@
+package com.vavatech.rest.restdemo.github;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GithubService {
+
+}
