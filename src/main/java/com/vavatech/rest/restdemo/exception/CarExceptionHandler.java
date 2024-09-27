adc45b87d41d4221417db8f9e9be5d6fbf321566
maniek <mpastuszka@gmail.com>
2024-09-27 11:46:34 +0200

    [add] - rest exception handler with switch pattern matching

--- src/main/java/com/vavatech/rest/restdemo/exception/CarExceptionHandler.java
+++ src/main/java/com/vavatech/rest/restdemo/exception/CarExceptionHandler.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class CarExceptionHandler {
 
-	@ExceptionHandler(CarNotFoundException.class)
-	public String handleCarNotFoundException(CarNotFoundException ex) {
-		return "not found:(";
+	@ExceptionHandler({CarNotFoundException.class, CarValidationException.class})
+	public String handleCarNotFoundException(RuntimeException ex) {
+
+		return switch (ex) {
+			case CarNotFoundException exception -> "car not found:(";
+			case CarValidationException exception -> "car validation failed:(";
+			case RuntimeException exception -> "unknown exception:(";
+		};
 	}
 }
---
+++ src/main/java/com/vavatech/rest/restdemo/exception/CarValidationException.java
@@ -0,0 +1,8 @@
+package com.vavatech.rest.restdemo.exception;
+
+public class CarValidationException extends RuntimeException {
+
+	public CarValidationException(String message) {
+		super(message);
+	}
+}
